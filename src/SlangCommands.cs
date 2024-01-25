eb99641237f4aa1f0f6650eac51660545598d633
A-lexC <alexc@valvesoftware.com>
2024-01-24 19:34:14 -0800

    Add support for ctrl+k/c and ctrl+k/u
    Added support for ctrl+k/c and ctrl+k/u. Supports single line and block comments. Based on commenthelper used on python tools and other uses.

---
+++ src/SlangCommands.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.Commanding;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Text.Editor.Commanding.Commands;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+
+namespace SlangClient
+{
+    static public class CommentOptions
+    {
+        public const string SingleLineCommentString = "//";
+        public const string BlockCommentStartString = "/*";
+        public const string BlockCommentEndString = "*/";
+    }
+
+    [Export(typeof(ICommandHandler))]
+    [Name(nameof(CommentSelection))]
+    [ContentType(SlangContentDefinition.SlangContentType)]
+    [TextViewRole(PredefinedTextViewRoles.PrimaryDocument)]
+    internal class CommentSelection : ICommandHandler<CommentSelectionCommandArgs>
+    {
+        
+        public string DisplayName => nameof(CommentSelection);
+
+        public bool ExecuteCommand(CommentSelectionCommandArgs args, CommandExecutionContext executionContext)
+        {
+            return CommentHelper.CommentOrUncommentBlock(args.TextView, comment: true);
+        }
+
+        public CommandState GetCommandState(CommentSelectionCommandArgs args)
+        {
+            return CommandState.Available;
+        }
+    }
+
+    [Export(typeof(ICommandHandler))]
+    [Name(nameof(UncommentSelection))]
+    [ContentType(SlangContentDefinition.SlangContentType)]
+    [TextViewRole(PredefinedTextViewRoles.PrimaryDocument)]
+
+    internal class UncommentSelection : ICommandHandler<UncommentSelectionCommandArgs>
+    {
+        public string DisplayName => nameof(UncommentSelection);
+
+        public bool ExecuteCommand(UncommentSelectionCommandArgs args, CommandExecutionContext executionContext)
+        {
+            return CommentHelper.CommentOrUncommentBlock(args.TextView, comment: false);
+        }
+
+        public CommandState GetCommandState(UncommentSelectionCommandArgs args)
+        {
+            return CommandState.Available;
+        }
+    }
+ }
